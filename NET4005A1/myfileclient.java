864d44a02e8f46f9b23e25a02c55d86c632ae1f6
Josh Smith <joshuasmith7278@gmail.com>
2023-09-18 19:05:49 -0400

    Working base Tutorial 1

---
+++ NET4005A1/myfileclient.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class myfileclient {
+    public static void main(String[] args){
+        //Parse command line arguments (Server IP, Server Port, Filename)
+        if(args.length != 3){
+            System.out.println("Incorrect amount of Arguments in Request");
+        }
+
+        //Establish a socket connection to the server
+
+        //Send Request to Server
+
+        //Process and Display Server Response
+
+        //If file is found, recieve and save the file
+
+
+    }
+
+
+}
+
+
+class SocketHandling extends Thread{
+
+}
