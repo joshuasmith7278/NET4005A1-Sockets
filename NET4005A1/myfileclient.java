518f6672b628c91e8cde69d3c58d49a53bfa6473
Josh Smith <joshuasmith7278@gmail.com>
2023-09-18 21:07:08 -0400

    Implemented basic client/server socket comms

--- NET4005A1/myfileclient.java
+++ NET4005A1/myfileclient.java
@@ -6,17 +6,11 @@ public class myfileclient {
         //Parse command line arguments (Server IP, Server Port, Filename)
         if(args.length != 3){
             System.out.println("Incorrect amount of Arguments in Request");
+        }else{
+            new SocketHandling(args[0], Integer.parseInt(args[1]), args[2]).start();
         }
 
-        //Establish a socket connection to the server
-
-        //Send Request to Server
-
-        //Process and Display Server Response
-
-        //If file is found, recieve and save the file
-
-
+    
     }
 
 
@@ -24,5 +18,51 @@ public class myfileclient {
 
 
 class SocketHandling extends Thread{
+    String serverHost;
+    int serverPort;
+    String filename;
+
+    private static DataInputStream inFromServer;
+    private static DataOutputStream outToServer;
+
+    SocketHandling(String s, int p, String f){
+        serverHost = s;
+        serverPort = p;
+        filename = f;
+    }
+
+
+    //With threads, call run method 
+    public void run(){
+        try{
+            //Establish a socket connection to the server
+            Socket socket = new Socket(serverHost, serverPort);
+            inFromServer = new DataInputStream(socket.getInputStream());
+            outToServer = new DataOutputStream(socket.getOutputStream());
+
+
+
+            //Send Request to Server
+            outToServer.writeUTF(filename);
+
+            //Process and Display Server Response
+            System.out.println(inFromServer.readUTF());
+
+            //If file is found, recieve and save the file
+
+
+            //Catch any errors with connecting to the socket. Print any errors
+        }catch(IOException ex){
+            System.out.println(ex);
+        }
+
+
+
+    }
+
+     
+
+    
+
 
 }
--- NET4005A1/myfileserver.java
+++ NET4005A1/myfileserver.java
@@ -1,20 +1,89 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class myfileserver {
     public static void main(String[] args){
         //Create Server socket and listen for connections
+        int port = 8000;
 
-        //Create thread pol for client requests
+        try{
+            ServerSocket server = new ServerSocket(port);
 
-        while(true){
-            //Implement logic to handle single client request
+            //Create thread pol for client requests
+
+            while(true){
+                //Implement logic to handle single client request
+                Socket client = server.accept();
+                WorkerThread thread = new WorkerThread(client);
+                thread.start();
+            }
+
+        }catch(IOException ex){
+            System.out.println(ex);
         }
+        
     }
 }
 
 class WorkerThread extends Thread {
     //Implememnt handle single client request including stats and transferring files
+    Socket client;
+
+    WorkerThread(Socket c){
+        client = c;
+    }
+
+    public void run(){
+        InetAddress inet = client.getInetAddress();
+        System.out.println("Client hostname : " + inet.getHostAddress());
+        System.out.println("Client IP : " + inet.getHostAddress());
+        try{
+            DataInputStream inFromClient = new DataInputStream(client.getInputStream());
+            DataOutputStream outToClient = new DataOutputStream(client.getOutputStream());
+            outToClient.writeUTF("TCP Response from Server");
+            System.out.println(inFromClient.readUTF());
+
+
+        }catch(IOException ex){
+            System.out.println(ex);
+        }
+    
+       
+
+    }
 
 }
+
+
+
+
+/* 
+ class multiThreadServer {
+    public static void main(String[] args){
+        BlockingQueue queue = new ArrayBlockingQueue<>(100);
+
+        ThreadPoolExecutor executorpool = new ThreadPoolExecutor(10, 10, 10, TimeUnit.SECONDS, queue);
+
+        executorpool.prestartAllCoreThreads();
+
+       
+        for(int i = 0; i < 10; i++){
+            queue.offer(new WorkerThread(String.valueOf(i)));
+            
+        }
+
+        executorpool.shutdown();
+    }
+}
+
+
+*/
+
+ 
 
